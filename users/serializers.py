9ff64259824cba021e048e073ad49f642da1095b
naomitesfe <tesfayenuhamin@gmail.com>
2025-08-07 15:57:17 +0300

    feat: implemented user registratin api

--- users/serializers.py
+++ users/serializers.py
@@ -2,7 +2,25 @@ from rest_framework import serializers
 from .models import CustomUser
 from django.contrib.auth.password_validation import validate_password
 from rest_framework.validators import UniqueValidator
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'username', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
--- users/views.py
+++ users/views.py
@@ -1,11 +1,26 @@
+# users/views.py
 from rest_framework import generics
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .serializers import RegisterSerializer
-from .models import CustomUser
+from django.contrib.auth import get_user_model 
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.serializers import ModelSerializer
+from rest_framework.views import APIView
+from .serializers import RegisterSerializer
+
+User = get_user_model()
+class RegisterSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+       return User.objects.create_user(**validated_data)
+
 
 
 class RegisterView(generics.CreateAPIView):
-    queryset = CustomUser.objects.all()
-    permission_classes = [AllowAny]
+    queryset = User.objects.all()
     serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
